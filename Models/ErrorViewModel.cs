7a30dafbee82ff51f0924a85adbda387854f1568
sayalijadhav091 <sayali.jadhav091@gmail.com>
2023-04-11 14:25:32 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleDotNetWebapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
