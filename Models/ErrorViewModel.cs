db7e1dadacaa34bf01b27a8ef7ed4574c7a24d62
Sayali Jadhav <121858077+sayalijadhav091@users.noreply.github.com>
2023-04-13 14:29:09 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace SampleDotNetWebapp.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
