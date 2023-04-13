f94adc2d6e00c147b70d636e81bc84f3c18ea41c
Sayali Jadhav <121858077+sayalijadhav091@users.noreply.github.com>
2023-04-13 14:58:33 +0530

    Update Privacy.cshtml.cs

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 namespace SampleDotNetWebapp.Pages;
-public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class PrivacyModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<PrivacyModel> _logger;
 
-    public void OnGet()
-    {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
